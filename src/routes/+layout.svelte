2676eb9cdbcf1716082d3e1c2cbcb0ce70efe3ac
Douglas Poma <douglaspoma@yahoo.com>
2023-11-25 15:13:38 -0300

    feat: create routes and layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<h1>Meu site</h1>
+
+<ul>
+  <li>
+    <a href="/">Home</a>
+  </li>
+  <li>
+    <a href="/sobre">Sobre</a>
+  </li>
+  <li>
+    <a href="/portfolio">Portfolio</a>
+  </li>
+  <li>
+    <a href="/contato">Contato</a>
+  </li>
+</ul>
+<slot />
