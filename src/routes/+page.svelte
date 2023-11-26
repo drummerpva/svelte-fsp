3cd5be62895b4e3a11aba8204a3a7f5c44d9ff42
Douglas Poma <douglaspoma@yahoo.com>
2023-11-26 20:13:07 -0300

    feat: create first component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+  import Button from './Button.svelte'
 </script>
 
 <h2>Home</h2>
+<Button />
+<Button label="Botão 2" />
+<Button />
+<Button label="Outro botão" />
---
+++ src/routes/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let label = 'Vazio'
+</script>
+
+<button class="btn">{label}</button>
+
+<style>
+  .btn {
+    color: #f00;
+    padding: 16px;
+    border: 1px solid #f00;
+    border-radius: 12%;
+  }
+</style>
