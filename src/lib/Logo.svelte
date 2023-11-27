67f9ebc829f49592ebb030f03281a9368281b301
Douglas Poma <douglaspoma@yahoo.com>
2023-11-27 10:53:59 -0300

    feat: quebrando componentes, logo, menu, footer

---
+++ src/lib/Logo.svelte
@@ -0,0 +1,7 @@
+<img src="images/logo.png" alt="logo" class="header-logo mr-5" />
+
+<style>
+  .header-logo {
+    height: 2rem;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Footer from '$lib/Footer.svelte'
   import Header from '$lib/Header.svelte'
   import '../app.pcss'
 </script>
 
 <Header />
 <slot />
+<Footer />
