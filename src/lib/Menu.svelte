ba6b302cc929d4cfeea8efb44eb15b6d8092bd0f
Douglas Poma <douglaspoma@yahoo.com>
2023-11-28 10:11:02 -0300

    feat: usando store global page

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { page } from '$app/stores'
   import Logo from './Logo.svelte'
   const menuItems = [
     { href: '/', label: 'Home' },
@@ -13,14 +14,17 @@
   <Logo />
 </a>
 {#each menuItems as { href, label }}
-  <a
-    {href}
-    class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-    >{label}</a
-  >
-  <!-- <a
-    {href}
-    class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
-    >{label}</a
-  > -->
+  {#if href === $page.url.pathname}
+    <a
+      {href}
+      class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+      >{label}</a
+    >
+  {:else}
+    <a
+      {href}
+      class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+      >{label}</a
+    >
+  {/if}
 {/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,4 @@
 <script lang="ts">
-  const lista = [
-    { nome: 'Douglas', idade: 32 },
-    { nome: 'Elaine', idade: 25 },
-    { nome: 'Osvaldo', idade: 5 },
-  ]
 </script>
 
 <h1>Home</h1>
-{#each lista as { nome, idade }}
-  <p>{nome} tem {idade} anos</p>
-{/each}
